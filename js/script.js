5b5fc5ca7123fb1dba36d6003db633d4baaea822
Phani Chaitanya <phanichaitanya63@gmail.com>
2025-11-01 23:19:49 +0530

    Updated portfolio with 99AcreScrapper project

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 const typed = new Typed(".text", {
     strings: [
-      "build smart AI models",
-      "develop desktop applications",
-      "create beautiful mobile apps",
-      "craft tools with Python",
-      "build smart web scrapers",
-      "design modern websites"
+        "build smart AI models",
+        "develop desktop applications",
+        "create beautiful mobile apps",
+        "craft tools with Python",
+        "build smart web scrapers",
+        "design modern websites"
     ],
     typeSpeed: 50,
     backSpeed: 40,
